ba389bcc5eaf8dfc81603d5b5eefefd236832b35
MM1212 <mm.jogos.game@gmail.com>
2023-05-28 23:24:04 +0100

    fix(strncmp): seg fault checks

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: martiper <martiper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 22:32:24 by mm                #+#    #+#             */
-/*   Updated: 2023/05/15 02:16:17 by martiper         ###   ########.fr       */
+/*   Updated: 2023/05/28 23:23:46 by martiper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
-	if ((!s1 && !s2) || !n)
+	if (!s1 || !s2 || !n)
 		return (0);
 	i = 0;
 	while (i < n && s1[i] == s2[i] && s1[i] && s2[i])
