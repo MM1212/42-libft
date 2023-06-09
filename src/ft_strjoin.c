c9a3132ca2c835a73e2c2993d44abe099d52ad14
MM1212 <mm.jogos.game@gmail.com>
2023-06-09 13:44:53 +0100

    fix(strjoin): ignore NULL params to the join

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: martiper <martiper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 11:47:05 by mm                #+#    #+#             */
-/*   Updated: 2023/04/23 07:01:56 by martiper         ###   ########.fr       */
+/*   Updated: 2023/06/09 13:38:04 by martiper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,21 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*join;
 	size_t	s1_length;
 	size_t	s2_length;
-	size_t	length;
+	size_t	idx;
 
-	if (!s2)
-		return (ft_strdup(s1));
+	idx = 0;
 	s1_length = ft_strlen(s1);
 	s2_length = ft_strlen(s2);
-	length = s1_length + s2_length;
-	join = malloc((length + 1) * sizeof(char));
+	join = malloc((s1_length + s2_length + 1) * sizeof(char));
 	if (!join)
 		return (NULL);
-	ft_strlcpy(join, s1, s1_length + 1);
-	ft_strlcpy(join + s1_length, s2, s2_length + 1);
-	join[length] = '\0';
+	if (s1)
+	{
+		ft_strlcpy(join, s1, s1_length + 1);
+		idx += s1_length;
+	}
+	if (s2)
+		ft_strlcpy(join + idx, s2, s2_length + 1);
+	join[idx] = '\0';
 	return (join);
 }
--- src/gnl/get_next_line_utils.c
+++ src/gnl/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: martiper <martiper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 21:21:25 by mm                #+#    #+#             */
-/*   Updated: 2023/05/07 00:23:28 by martiper         ###   ########.fr       */
+/*   Updated: 2023/06/09 13:38:58 by martiper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,9 @@
 
 char	*ft_strjoin_free(char *s1, char *s2)
 {
-	char	*join;
-	size_t	s1_length;
-	size_t	s2_length;
-	size_t	length;
+	char	*str;
 
-	if (!s1 || !s2)
-		return (NULL);
-	s1_length = ft_strlen(s1);
-	s2_length = ft_strlen(s2);
-	length = s1_length + s2_length;
-	join = malloc((length + 1) * sizeof(char));
-	if (!join)
-	{
-		free(s1);
-		return (NULL);
-	}
-	ft_strlcpy(join, s1, s1_length + 1);
-	ft_strlcpy(join + s1_length, s2, s2_length + 1);
-	join[length] = '\0';
+	str = ft_strjoin(s1, s2);
 	free(s1);
-	return (join);
+	return (str);
 }
