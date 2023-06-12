c5216099c8c345f2d27821009817c517b8e9e916
MM1212 <mm.jogos.game@gmail.com>
2023-06-12 05:29:45 +0100

    tweak(strjoin): use lcat

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: martiper <martiper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 11:47:05 by mm                #+#    #+#             */
-/*   Updated: 2023/06/10 18:05:12 by martiper         ###   ########.fr       */
+/*   Updated: 2023/06/12 05:27:25 by martiper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*join;
 	size_t	s1_length;
 	size_t	s2_length;
-	size_t	idx;
 
-	idx = 0;
 	s1_length = ft_strlen(s1);
 	s2_length = ft_strlen(s2);
-	join = malloc((s1_length + s2_length + 1) * sizeof(char));
+	join = ft_calloc((s1_length + s2_length + 1), sizeof(char));
 	if (!join)
 		return (NULL);
 	if (s1)
-	{
 		ft_strlcpy(join, s1, s1_length + 1);
-		idx += s1_length;
-	}
 	if (s2)
-	{
-		ft_strlcpy(join + idx, s2, s2_length + 1);
-		idx += s2_length;
-	}
-	join[idx] = '\0';
+		ft_strlcat(join, s2, s1_length + s2_length + 1);
 	return (join);
 }
