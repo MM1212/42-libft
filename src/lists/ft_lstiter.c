465f4d5eac9fcb777f358f7ff8ea9fbeddb66957
MM1212 <mm.jogos.game@gmail.com>
2024-03-23 21:37:42 +0000

    fix(lstiter): whoops

--- src/lists/ft_lstiter.c
+++ src/lists/ft_lstiter.c
@@ -6,19 +6,30 @@
 /*   By: martiper <martiper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 20:19:32 by mm                #+#    #+#             */
-/*   Updated: 2023/05/17 09:14:16 by martiper         ###   ########.fr       */
+/*   Updated: 2024/03/23 21:37:31 by martiper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(void *))
+void	ft_lstiter(t_list *lst, t_lst_iter f)
 {
 	if (!f)
 		return ;
 	while (lst)
 	{
 		f(lst->content);
+		lst = lst->next;
+	}
+}
+
+void	ft_lstiter2(t_list *lst, t_lst_iter2 f, void *data)
+{
+	if (!f)
+		return ;
+	while (lst)
+	{
+		f(lst->content, data);
 		lst = lst->next;
 	}
 }
