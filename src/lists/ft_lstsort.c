f9d5af34d875fac8e218f9e408b271d2cb788697
MM1212 <mm.jogos.game@gmail.com>
2024-03-26 17:27:03 +0000

    refactor(lst/sort): change from bubble to quick

--- src/lists/ft_lstsort.c
+++ src/lists/ft_lstsort.c
@@ -6,64 +6,72 @@
 /*   By: martiper <martiper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 11:52:10 by martiper          #+#    #+#             */
-/*   Updated: 2024/03/25 22:09:21 by martiper         ###   ########.fr       */
+/*   Updated: 2024/03/26 17:26:30 by martiper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
-	Sorts a double linked list (with next & prev pointers)
-	Callback returns 1, 0 or -1
- */
-void	ft_lstsort(t_list *lst, t_lst_cmp fn)
+static void	swap(void **a, void **b)
 {
-	t_list	*tmp;
-	void	*content;
+	void	*t;
 
-	if (!lst || !fn)
-		return ;
-	while (lst)
-	{
-		tmp = lst->next;
-		while (tmp)
-		{
-			if (fn(lst->content, tmp->content) > 0)
-			{
-				content = lst->content;
-				lst->content = tmp->content;
-				tmp->content = content;
-			}
-			tmp = tmp->next;
-		}
-		lst = lst->next;
-	}
+	t = *a;
+	*a = *b;
+	*b = t;
 }
 
-void	ft_lstsort2(\
-	t_list *lst, \
-	t_lst_cmp2 fn, \
+static t_list	*partition(t_list *l, t_list *h, t_lst_cmp2 cmp, void *data)
+{
+	t_list	*i;
+	t_list	*j;
+
+	i = l->prev;
+	j = l;
+	while (j != h)
+	{
+		if (cmp(j->content, h->content, data) < 0)
+		{
+			if (!i)
+				i = l;
+			else
+				i = i->next;
+			swap(&(i->content), &(j->content));
+		}
+	}
+	if (!i)
+		i = l;
+	else
+		i = i->next;
+	swap(&(i->content), &(h->content));
+	return (i);
+}
+
+static void	quick_sort(\
+	t_list *left, \
+	t_list *right, \
+	t_lst_cmp2 cmp, \
 	void *data \
 )
 {
-	t_list	*tmp;
-	void	*content;
+	t_list	*pivot;
 
-	if (!lst || !fn)
+	if (right == NULL || left == right || left == right->next)
 		return ;
-	while (lst)
-	{
-		tmp = lst->next;
-		while (tmp)
-		{
-			if (fn(lst->content, tmp->content, data) > 0)
-			{
-				content = lst->content;
-				lst->content = tmp->content;
-				tmp->content = content;
-			}
-			tmp = tmp->next;
-		}
-		lst = lst->next;
-	}
+	pivot = partition(left, right, cmp, data);
+	quick_sort(left, pivot->prev, cmp, data);
+	quick_sort(pivot->next, right, cmp, data);
+}
+
+void	ft_lstsort2(t_list *lst, t_lst_cmp2 cmp, void *data)
+{
+	t_list	*last;
+
+	last = ft_lstlast(lst);
+	quick_sort(lst, last, cmp, data);
+}
+
+void	ft_lstsort(t_list *lst, t_lst_cmp fn)
+{
+	ft_lstsort2(lst, (t_lst_cmp2)fn, NULL);
 }
