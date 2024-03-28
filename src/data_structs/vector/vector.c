8fb23146d7da8084416520c9b9fa61c62dc52080
MM1212 <mm.jogos.game@gmail.com>
2024-03-28 17:55:34 +0000

    tweak(dst/vector): allow for vec construction with size of 0

--- src/data_structs/vector/vector.c
+++ src/data_structs/vector/vector.c
@@ -6,7 +6,7 @@
 /*   By: martiper <martiper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 19:10:09 by martiper          #+#    #+#             */
-/*   Updated: 2024/03/26 22:51:05 by martiper         ###   ########.fr       */
+/*   Updated: 2024/03/28 17:53:54 by martiper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ t_vector	*vector_create3(\
 	handle->capacity = capacity;
 	handle->deletef = delete_fn;
 	handle->data = ft_calloc(capacity, elem_size);
-	if (!handle->data)
+	if (!handle->data && capacity > 0)
 	{
 		free(handle);
 		return (NULL);
